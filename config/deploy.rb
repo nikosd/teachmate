4325406b9e4b4f883e4467e5b02cce16e9a0327a
Roman Snitko <subscribe@snitko.ru>
2008-09-08 11:57:58 +0400

    Updating path to jiff_auth repo in .submodules

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,8 +5,6 @@ set :application, "teachmate.org"
 # via the :deploy_to variable:
 # set :deploy_to, "/var/www/#{application}"
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
 set :scm, :git
 set :branch, "master"
 set :repository, "/home/roman/tm.git"
@@ -18,3 +16,16 @@ set :user, "roman"
 role :app, "test-server"
 role :web, "test-server"
 role :db,  "test-server", :primary => true
+
+# custom tasks
+after "deploy", "init_submodules"
+
+task :init_submodules, :roles => :app do
+  run "cd #{current_path}; git submodule init; git submodule update"
+end
+
+namespace(:deploy) do
+  task :restart do
+    # nothing here yet
+  end
+end
