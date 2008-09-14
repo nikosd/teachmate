1eec4745aa2940a8fca3ae70dd8e4a1e17660472
Roman Snitko <subscribe@snitko.ru>
2008-09-14 22:47:58 +0400

    Added '#edit_comment' form animation

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,5 +4,9 @@ $(function() {
     $(":submit",this).attr("disabled", "disabled");
   });
 
+  $("#comments-box #my_comment a").bind("click", function(event) {
+    $(this).parent().parent().empty();
+    $("#edit_comment").show('slow');
+  });
 
 });
