1b5f672da34efbaae832ff47c33e7789796d6c41
Roman Snitko <subscribe@snitko.ru>
2008-09-15 17:16:28 +0400

    Adding login from any page with '#loginbox'

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,14 +6,25 @@ $(function() {
 
   $("#comments-box #my_comment a").bind("click", function(event) {
     $(this).parent().parent().empty();
-    $("#edit_comment").show('slow');
+    $("#edit_comment").show("slow");
     event.preventDefault();
   });
 
   $("a[@href*=show_comments]").bind("click", function(event) {
-    $('#comments').show('slow');
-    $(this).hide('slow');
+    $("#comments").show("slow");
+    $(this).hide("slow");
     event.preventDefault();
   });
+
+  $("a[@href^=/login]").toggle(
+    function(event) {
+      $("#loginbox").css("left", event.pageX - 10).css("top", event.pageY + 15).show("slow");
+      event.preventDefault();
+    },
+    function(event) {
+      $("#loginbox").fadeOut();
+      event.preventDefault();
+    }
+  );
 
 });
