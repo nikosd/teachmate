7964ec6196b097533f62f5cd517813776f37abb4
Roman Snitko <subscribe@snitko.ru>
2008-09-24 22:36:24 +0400

    Remove the body of the signup method in UserMailer

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,11 +1,7 @@
 class UserMailer < ActionMailer::Base
 
     def signup(user)
-      @subject      = "Signup confirmation"
-      @recipients   = user.email
-      @from         = "no-reply@teachmate.org"
-      @sent_on      = Time.now
-      @body         = {:id => user.id, :password => user.password}
+
     end
 
     def lost_password(user)
