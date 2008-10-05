c090366c009d48bae1504abc04f16cf84ed7f3c7
Roman Snitko <subscribe@snitko.ru>
2008-10-05 22:55:48 +0400

    Correct algorithm for SubscriptionMailer

--- lib/subscription_mailer.rb
+++ lib/subscription_mailer.rb
@@ -1,9 +1,10 @@
 class SubscriptionMailer
 
-  attr_reader :subscriptions, :search_queries
+  attr_reader :subscriptions, :search_queries, :last_run
 
   def find_subscriptions(time_ago)
     @subscriptions = Subscription.find(:all, :conditions => ['updated_at < (?)', time_ago], :order => 'updated_at')
+    @subscriptions_size = @subscriptions.size
   end
 
   def find_search_queries
@@ -16,29 +17,141 @@ class SubscriptionMailer
     search_queries_ids.uniq!
 
     @search_queries = SearchQuery.find(:all, :conditions => ['id in (?)', search_queries_ids]).compact 
-    @search_queries.each {|sq| sq.per_page=10; sq.run; sq}
+#     @search_queries.each {|sq| sq.per_page=10; sq.run; sq}
   end
 
   def slice(options)
-    number_of_mails = @subscriptions.size/options[:divider]
-    leftover = @subscriptions.size%options[:divider]
-    number_of_mails += leftover if leftover > 0
+    read_last_run
+    
+      if @subscriptions_size > 0
+      # n1      - subscriptions that were fired on previous run
+      # m1      - what's left of previous run
+      # n2      - subscriptions we're going to fire this time
+      # m2      - total subsciptions this time
+      # divider - the number we divide for all subscriptions
+      #
+      # The following code says this:
+      #   
+      #   if m1 < m2
+      #     new = m2 - m1
+      #     n2 = n1 + (new/divider)
+      #   else
+      #     n2 = n1
+      #   end
+      #
+      # This way we always stay updated and fire as much mails
+      # as we currently need.
+      @last_run[:mails_sent] = 0 if @last_run[:mails_left] == 0
+      # if number of subscriptions increased since last run
+      if @last_run[:mails_left] < @subscriptions_size
+        @number_of_mails = 
+        @last_run[:mails_sent] + (@subscriptions_size - @last_run[:mails_left])/options[:divider]
+        leftover = (@subscriptions_size - @last_run[:mails_left])%options[:divider]
 
-    @subscriptions = @subscriptions.slice(0..number_of_mails-1)
-  end
-
-  def mail
-    @subscriptions.each do |s|
-      found_users = nil
-      @search_queries.each do |sq|
-        if sq.id == s.search_query_id
-          found_users = sq.users
-          break
-        end
+      # if number of subscription stayed the same or even decreased
+      else
+        @number_of_mails = @last_run[:mails_sent]
+        leftover = 0
       end
-      UserMailer.deliver_search_subscription(found_users) unless found_users.blank?
+      
+      @number_of_mails += leftover if leftover > 0
+
     end
   end
 
+  def mail
+
+    # Reading last_search_query if from file
+    last_search_query = @last_run[:last_search_query]
+
+    current_search_query = @search_queries.first
+    @search_queries.each do |sq|
+      current_search_query = sq if last_search_query == sq.id
+    end if last_search_query
+    current_search_query.run
+
+    # collecting subscriptions for current search_query
+    # limiting their number to @number_of_mails
+    mails = []
+
+    while mails.size < @number_of_mails
+
+      # if there's no more subscriptions for the current searchquery
+      # switch to the next query
+      subscriptions_to_delete = []
+      @subscriptions.each_index do |i|
+        break if mails.size >= @number_of_mails
+          if @subscriptions[i].search_query_id == current_search_query.id
+            mails << {:subscription => @subscriptions[i], :results => current_search_query.users}
+            subscriptions_to_delete << i
+          end
+        
+        # this fires on the last run
+        if i == @subscriptions.size - 1
+          @search_queries.delete(current_search_query)
+          current_search_query = @search_queries.first
+          subscriptions_to_delete.each { |index| @subscriptions.delete_at(index) }
+        end
+      end
+
+      break if @subscriptions.empty?
+    end
+
+
+    # collecting users for all mails
+    user_ids = []
+    mails.each { |m| user_ids <<  m[:subscription].user_id }
+    users = User.find(:all, :conditions => ['id in (?)', user_ids])
+    
+    # adding user models to mails' each element's hash
+    mails.each_index do |i|
+      users.each do |u|
+        if u.id == mails[i][:subscription].user_id
+          #puts "Sending search results to #{u.email}"
+          UserMailer.deliver_search_subscription(
+            :user => u, :results => mails[i][:results]
+          )
+          Subscription.record_timestamps = false
+          mails[i][:subscription].update_attribute(:updated_at, Time.now)
+          Subscription.record_timestamps = true
+        end
+      end
+    end
+
+    if current_search_query
+      last_search_query = current_search_query.id
+    else
+      last_search_query = nil
+    end
+    write_last_run(
+      :last_search_query => last_search_query,
+      :mails_sent => mails.size,
+      :mails_left => @subscriptions_size - mails.size
+    )
+
+  end 
+
+  def read_last_run
+    # defaults
+    @last_run = {:mails_sent => 0, :mails_left => 0}
+    
+    last_run_log = File.readlines("#{RAILS_ROOT}/log/subscription_mailer_last_run.log")
+    last_run_log.each do |element|
+      option = element.chomp.split(': ')
+      @last_run.merge!({option[0].to_sym => option[1].to_i}) unless option.blank?
+    end
+    @last_run
+  end
+
+  def write_last_run(options)
+    file = File.open("#{RAILS_ROOT}/log/subscription_mailer_last_run.log", 'w')
+    options.each do |k,v|
+      if v
+        file.puts "#{k}: #{v}";
+        @last_run[k] = v
+      end
+    end
+    file.close
+  end
 
 end
--- spec/lib/subscription_mailer_spec.rb
+++ spec/lib/subscription_mailer_spec.rb
@@ -1,6 +1,10 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 require 'subscription_mailer'
 
+class SubscriptionMailer
+  attr_reader :number_of_mails
+end
+
 describe SubscriptionMailer do
 
   scenario :subscription_mailer, :root => false
@@ -30,7 +34,7 @@ describe SubscriptionMailer do
   it "should slice the oldest subscriptions" do
     oldest_before_slice = @s_mailer.subscriptions.first
     @s_mailer.slice(:divider => 24)
-    @s_mailer.should have(20).subscriptions #10 + 10 from leftover
+    @s_mailer.should have(250).subscriptions #10 + 10 from leftover
     @s_mailer.subscriptions.first.should == oldest_before_slice
   end
 
@@ -49,6 +53,77 @@ describe SubscriptionMailer do
     @s_mailer.slice(:divider => 24)
     @s_mailer.find_search_queries
     @s_mailer.mail
+
+    new_s_mailer = SubscriptionMailer.new
+    new_s_mailer.find_subscriptions(1.day.ago)
+    new_s_mailer.should have(230).subscriptions 
+  end
+
+  it "should send exactly @number_of_emails unless it runs out of @search_queries" do
+
+    UserMailer.should_receive(:deliver_search_subscription).exactly(250).times
+
+    @s_mailer.slice(:divider => 2)
+    @s_mailer.find_search_queries
+    @s_mailer.mail
+
+    last_run = @s_mailer.read_last_run
+    last_run[:last_search_query].should == 2
+    last_run[:mails_sent].should == 125
+    last_run[:mails_left].should == 125
+
+    new_s_mailer = SubscriptionMailer.new
+    new_s_mailer.find_subscriptions(1.day.ago)
+    new_s_mailer.slice(:divider => 2)
+    new_s_mailer.find_search_queries
+    new_s_mailer.mail
+
+    last_run = new_s_mailer.read_last_run
+    last_run[:last_search_query].should be_nil
+    last_run[:mails_sent].should == 125
+    last_run[:mails_left].should == 0
+
+  end
+
+  it "should handle increasing number of subscriptions" do
+
+    UserMailer.should_receive(:deliver_search_subscription).any_number_of_times
+    
+
+    # run 1
+    @s_mailer.slice(:divider => 3)
+    @s_mailer.find_search_queries
+    @s_mailer.mail
+
+    @s_mailer.last_run[:mails_sent].should == 84
+    @s_mailer.last_run[:mails_left].should == 166
+
+    # run 2 - now adding 30 new subscriptions
+    
+    users = User.find(:all, :limit => 30)
+    added_subscriptions = []
+    users.each do |u|
+      subscription = u.subscriptions.create(:search_query_id => 1).id
+      if subscription
+        Subscription.record_timestamps = false      
+        Subscription.find(subscription).update_attribute(:updated_at, 1.day.ago - 1.minutes)
+        Subscription.record_timestamps = true
+      end
+    end
+    
+
+    @s_mailer.find_subscriptions(1.day.ago)
+    @s_mailer.slice(:divider => 3)
+    @s_mailer.find_search_queries
+    @s_mailer.mail
+
+    @s_mailer.last_run[:mails_sent].should == 95
+    @s_mailer.last_run[:mails_left].should == 100
+
+  end
+
+  after(:each) do
+    File.open("#{RAILS_ROOT}/log/subscription_mailer_last_run.log", 'w').close
   end
 
 end
