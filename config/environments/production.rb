e7aed65aa4de62fa872b1b4fe625001f3651156d
Roman Snitko <subscribe@snitko.ru>
2008-10-07 16:08:57 +0400

    Showing notes in search results

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ config.action_view.cache_template_loading            = true
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
 
-ActionMailer::Base.delivery_method = :test
+ActionMailer::Base.delivery_method = :sendmail
 ActionMailer::Base.default_charset = 'utf-8'
 
 AVATARS_PATH = "/var/www/teachmate/shared/public/images/avatars"
