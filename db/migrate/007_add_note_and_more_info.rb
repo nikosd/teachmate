9c786f41a5b52ed81875cf4fb696f98b74e6926a
Roman Snitko <subscribe@snitko.ru>
2008-10-08 18:07:56 +0400

    Removing limit from :more_info

--- db/migrate/007_add_note_and_more_info.rb
+++ db/migrate/007_add_note_and_more_info.rb
@@ -1,8 +1,8 @@
 class AddNoteAndMoreInfo < ActiveRecord::Migration
   def self.up
-    remove_column :users, :notes, :more_info
+    remove_column :users, :notes
     add_column :users, :notes, :string, :limit => 100
-    add_column :users, :more_info, :text, :limit => 100
+    add_column :users, :more_info, :text
   end
 
   def self.down
