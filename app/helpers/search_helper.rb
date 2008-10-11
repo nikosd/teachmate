11acb0f42c6358a2e158c92ad25312e408e2c69f
Roman Snitko <subscribe@snitko.ru>
2008-10-11 16:16:45 +0400

    Limit number of charachters for string output in search

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -6,6 +6,7 @@ module SearchHelper
 		@search.tags.each do |tag|
 			tag.send("#{tagtype.to_s}_taggings").each do |tagging|
 				if tagging.user_id == user.id  
+          tag.string = tag.string.slice(0..24) + '...' if tag.string.length > 25
           if @search.send("#{tagtype.to_s}_tags").include?(tag)
             tags_matched << "<strong>#{tag.string}</strong>" unless tags_matched.size >= 10
           else
@@ -17,7 +18,7 @@ module SearchHelper
 		end
     
     result =  tags_matched.concat(tags)
-    more = '...' if result.size > 10
+    more = ', [...]' if result.size > 10
     "#{result.slice(0..9).join(', ')} #{more}"
 	end
 
