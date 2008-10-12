63401c1f08157e23237b56d265466b627bc001ec
Roman Snitko <subscribe@snitko.ru>
2008-10-12 16:45:02 +0400

    Adding expiry check on each fragment

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -10,14 +10,35 @@ class SearchController < ApplicationController
     @query_params = {
       :learn => @search.teach_string,
       :teach => @search.learn_string,
-      :location  => @search.location
+      :location  => @search.location,
+      :page      => params[:page]
     }
+    @query_params.delete(:page) if @query_params[:page].to_i == 1
 
-    unless read_fragment(@query_params)
+    when_fragment_expired(@query_params, 1.minutes.ago) do  
       @search.run
       @users = @search.users if @search.errors.empty?
     end
 
 	end
+
+  def when_fragment_expired(fragment, expiry_time)
+    if fragment_exist?(@query_params)
+
+      cache_file = File.join(
+        *Digest::MD5.hexdigest(fragment_cache_key(fragment)
+      ).scan(/(.{2})(.{2})(.{2})(.*)/)) + '.cache'
+
+      cache_file_ctime = File.ctime("#{RAILS_ROOT}/public/cache/#{cache_file}")
+
+      if cache_file_ctime < expiry_time
+        expire_fragment(fragment)
+        yield
+      end
+
+    else
+      yield
+    end
+  end
 
 end
--- lib/advanced_file_store_caching.rb
+++ lib/advanced_file_store_caching.rb
@@ -4,7 +4,12 @@ class AdvancedFileStore < ActiveSupport::Cache::FileStore
   private
 
   def real_file_path(name)
-    @cache_path + '/' + File.join(*Digest::MD5.hexdigest(name).scan(/(.{2})(.{2})(.{2})(.*)/)) + '.cache'
+
+    # ensures meta file lies in the same dir as .cache file 
+    suffix = '.cache'
+    suffix = '.meta' if name.gsub!(/_meta\Z/, '')
+
+    @cache_path + '/' + File.join(*Digest::MD5.hexdigest(name).scan(/(.{2})(.{2})(.{2})(.*)/)) + suffix
   end
 
 end
