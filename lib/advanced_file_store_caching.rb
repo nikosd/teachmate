75b3f101aa84d477e08467a084b8fe226a4f7fbb
Roman Snitko <subscribe@snitko.ru>
2008-10-13 20:29:05 +0400

    IE font-size bug with hints fixed

--- lib/advanced_file_store_caching.rb
+++ lib/advanced_file_store_caching.rb
@@ -4,12 +4,7 @@ class AdvancedFileStore < ActiveSupport::Cache::FileStore
   private
 
   def real_file_path(name)
-
-    # ensures meta file lies in the same dir as .cache file 
-    suffix = '.cache'
-    suffix = '.meta' if name.gsub!(/_meta\Z/, '')
-
-    @cache_path + '/' + File.join(*Digest::MD5.hexdigest(name).scan(/(.{2})(.{2})(.{2})(.*)/)) + suffix
+    @cache_path + '/' + File.join(*Digest::MD5.hexdigest(name).scan(/(.{2})(.{2})(.{2})(.*)/)) + '.cache'
   end
 
 end
